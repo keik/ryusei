d08434b6dd78f44a0c496191b77a703769646bcf
keik <k4t0.kei@gmail.com>
2015-08-14 22:11:42 +0900

    SAY IT NOW

---
+++ src/main/java/info/keik/ryusei/App.java
@@ -0,0 +1,8 @@
+package info.keik.ryusei;
+
+public class App {
+
+    public static void main(String[] args) {
+        R.Y.U.S.E.I ryusei = new R().new Y().new U().new S().new E().new I();
+    }
+}
---
+++ src/main/java/info/keik/ryusei/R.java
@@ -0,0 +1,17 @@
+package info.keik.ryusei;
+
+public class R {
+    class Y {
+        class U {
+            class S {
+                class E {
+                    class I {
+                        {
+                            System.out.println("ｾｨｲｯ! (ﾅ) ﾌｯﾌｰ↑ｗｗｗ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
